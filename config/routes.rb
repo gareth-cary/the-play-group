30ee5040e1d4e148422db2a60585ab69cd537216
Gareth Cary <gareth@caryfamily.co.uk>
2020-06-19 20:04:39 +0100

    style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home2'
-  get 'home1', to: 'pages#home1'
+  root to: 'pages#home1'
+  get 'home2', to: 'pages#home2'
   get 'home3', to: 'pages#home3'
   get 'home4', to: 'pages#home4'
   get 'home5', to: 'pages#home5'
